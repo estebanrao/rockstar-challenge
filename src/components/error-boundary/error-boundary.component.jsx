be61ed26cdce0e164d8a209efcde9b32f31831c6
Esteban Rao <estebanrao@gmail.com>
2020-07-31 13:42:50 -0300

    Changes ErrorBoundary mesage

--- src/components/error-boundary/error-boundary.component.jsx
+++ src/components/error-boundary/error-boundary.component.jsx
@@ -16,7 +16,7 @@ export default class ErrorBoundary extends React.Component {
 
   render() {
     if (this.state.hasError) {
-      return <h1>Something went wrong.</h1>;
+      return <h1>Something went wrong. Try again in a few minutes.</h1>;
     }
 
     return this.props.children;
